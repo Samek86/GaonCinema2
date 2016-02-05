6f6dea738a3f20c09efd5ef0aba607a9f75a4f83
준호 <ttiis@naver.com>
2016-02-05 12:09:48 +0900

    수정중

--- src/main/java/com/gaon/cinema/movie/MovieController.java
+++ src/main/java/com/gaon/cinema/movie/MovieController.java
@@ -1,7 +1,10 @@
 package com.gaon.cinema.movie;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,9 +15,14 @@ public class MovieController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MovieController.class);
 	
+	@Autowired
+	private MovieDAO dao;
+	
 	@RequestMapping(value = "/movie.do", method = RequestMethod.GET)
 	public ModelAndView movie() {
 		ModelAndView mav = new ModelAndView();
+		List<MovieDTO> list = dao.dbNowMovie();
+		mav.addObject("nowmovie", list);
 		mav.addObject("page", "movie");
 		mav.setViewName("mainLayout");
 		return mav;
