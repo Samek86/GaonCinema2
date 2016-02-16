f134cd8777b5395a9773d087d0e8f111aac26518
창수 <operation666@naver.com>
2016-02-16 09:31:10 +0900

    servation

--- src/main/java/com/gaon/cinema/reservation/ReservationController.java
+++ src/main/java/com/gaon/cinema/reservation/ReservationController.java
@@ -2,21 +2,31 @@ package com.gaon.cinema.reservation;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.gaon.cinema.qna.QnaDTO;
+
 @Controller
 public class ReservationController {
+	
+	@Autowired
+	ReservationDAO dao;
 	
 	private static final Logger logger = LoggerFactory.getLogger(ReservationController.class);
 	
 	@RequestMapping(value = "/reservation.do", method = RequestMethod.GET)
 	public ModelAndView reservation() {
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("page", "reservation");
-		mav.setViewName("mainLayout");
-		return mav;
-	}
-}
+			ModelAndView mav = new ModelAndView();
+			
+			mav.addObject("movieList", dao.dbSelectMovieNameAge());
+			
+			mav.addObject("page", "reservation");
+			mav.setViewName("mainLayout");
+			return mav;
+			}//reservation
+	
+}//ReservationController class END
---
+++ src/main/java/com/gaon/cinema/reservation/ReservationShowDTO.java
@@ -0,0 +1,12 @@
+package com.gaon.cinema.reservation;
+
+public class ReservationShowDTO {
+	private String m_name;
+	private String age;
+	public String getM_name() {		return m_name;	}
+	public void setM_name(String m_name) {		this.m_name = m_name;  }
+	public String getAge() {		return age;	}
+	public void setAge(String age) {		this.age = age;	}
+
+}//ReservationShowDTO class END
+
